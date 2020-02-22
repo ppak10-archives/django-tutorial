a0a3866168ee168f01ad775d0e6be6e8d62ef8d7
Peter <ppak10@gmail.com>
2020-02-22 14:48:14 -0800

    Finishes part 3 of the Django tutorial

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,35 @@
 # Create your views here.
 
 from django.http import HttpResponse
+# from django.template import loader
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
 
 def index(request):
-  return HttpResponse("Hello World")
+  latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  # template = loader.get_template('polls/index.html')
+  context = {
+    'latest_question_list': latest_question_list,
+  }
+  return render(request, 'polls/index.html', context)
+  # return HttpResponse(template.render(context, request))
+
+  # output = ', '.join([q.question_test for q in latest_question_list])
+  # return HttpResponse("Hello World")
+
+def detail(request, question_id):
+  # try:
+  #   question = Question.objects.get(pk=question_id)
+  # except Question.DoesNotExist:
+  #   raise Http404("Question does not exist")
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/detail.html', {'question': question})
+  # return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+  response = "You're looking at the results of question %s."
+  return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+  return HttpResponse("You're voting on question %s." % question_id)
