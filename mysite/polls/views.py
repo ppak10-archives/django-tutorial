d9cd4952d59598ab817e4aecd9fd6b632e5a3153
Peter <ppak10@gmail.com>
2020-02-24 14:26:11 -0800

    Finishes Tutorial 5 - Testing

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,10 @@
 # from django.shortcuts import render
 
-# Create your views here.
 
 from django.http import HttpResponse, HttpResponseRedirect
-# from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -16,13 +15,14 @@ class IndexView(generic.ListView):
 
   def get_queryset(self):
     """Return the last five published questions."""
-    return Question.objects.order_by('-pub_date')[:5]
-
+    return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
   model = Question
   template_name = 'polls/detail.html'
 
+  def get_queryset(self):
+    return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
   model = Question
